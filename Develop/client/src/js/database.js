d27ea05848f45b465cfc06156423ad0bbc5124af
Aubrey McKinney <99014955+Shadowasders@users.noreply.github.com>
2023-08-03 14:39:39 -0400

    updated database.js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,25 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const textDb = await openDB('Jate', 1);
+  const txt = textDb.transaction('Jate','readwrite');
+  const store = txt.objectStore('Jate');
+  const request = store.put({ id: 1, Jate: content});
+  const result = request;
+  console.log('?? did it work?', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('get everything from database');
+  const textDb = await openDB('Jate', 1);
+  const txt = textDb.transaction('Jate', 'readonly');
+  const store = txt.objectStore('Jate');
+  const request = store.getAll();
+  const result = await result;
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
